d047adfda70050b46489ed33911f4865aaf6666d
Alexander Makarov <sam@rmcreative.ru>
2021-01-15 11:01:28 +0300

    Update src/TranslatorInterface.php
    Co-authored-by: Aleksei Gagarin <roxblnfk@ya.ru>

--- src/TranslatorInterface.php
+++ src/TranslatorInterface.php
@@ -10,7 +10,7 @@ namespace Yiisoft\Translator;
 interface TranslatorInterface
 {
     /**
-     * @param Category $category Add category.
+     * Add category.
      */
     public function addCategorySource(Category $category): void;
 
