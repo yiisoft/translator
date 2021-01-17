c252e6b6a0c2edcb0e994bc222081b0576907974
DarkDef <darkdef@gmail.com>
2021-01-17 21:45:00 +0300

    fixes

--- tests/TranslatorInstanceofTest.php
+++ tests/TranslatorInstanceofTest.php
@@ -4,17 +4,11 @@ declare(strict_types=1);
 
 namespace Yiisoft\Translator\Tests;
 
-use PHPUnit\Framework\TestCase;
 use Psr\EventDispatcher\EventDispatcherInterface;
-use Yiisoft\Translator\Category;
-use Yiisoft\Translator\Event\MissingTranslationCategoryEvent;
-use Yiisoft\Translator\Event\MissingTranslationEvent;
 use Yiisoft\Translator\Message;
-use Yiisoft\Translator\MessageFormatterInterface;
-use Yiisoft\Translator\MessageReaderInterface;
 use Yiisoft\Translator\Translator;
 
-final class TranslatorInstanceof  extends BaseMock
+final class TranslatorInstanceofTest  extends BaseMock
 {
 
     public function testTranslationInstanceof(): void {
@@ -27,7 +21,7 @@ final class TranslatorInstanceof  extends BaseMock
                     'en' => [
                         'test.id1' => 'translation test {id1}',
                         'test.id2' => 'translation test {id2} {id4}',
-                        'test.id3' => '{id3}'
+                        'test.id3' => '{id3}',
                     ],
                 ],
             ]),
@@ -41,7 +35,7 @@ final class TranslatorInstanceof  extends BaseMock
             'test.id2' => new Message(
                 'test.id2',
                 [
-                    'id2' => new Message('test.id3', ['id3' => '11'] ),
+                    'id2' => new Message('test.id3', ['id3' => '11']),
                     'id4' => 12,
                 ]
             ),
@@ -76,7 +70,7 @@ final class TranslatorInstanceof  extends BaseMock
                 new Message(
                     'test.id2',
                     [
-                        'id2' => new Message('test.id3', ['id3' => '11'] ),
+                        'id2' => new Message('test.id3', ['id3' => '11']),
                         'id4' => 12,
                     ]
                 ),
