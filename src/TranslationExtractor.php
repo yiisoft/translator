3011237047d22e36c88b950d065a8c5eec8dad67
DarkDef <darkdef@gmail.com>
2021-03-21 18:48:35 +0300

    change conditions

--- src/TranslationExtractor.php
+++ src/TranslationExtractor.php
@@ -247,7 +247,7 @@ final class TranslationExtractor
         while ($i < $countTokens && $tokens[$i] === '.') {
             if ($tokens[$i + 1][0] === T_CONSTANT_ENCAPSED_STRING) {
                 $fullMessage .= substr($tokens[$i + 1][1], 1, -1);
-            } elseif ($tokens[$i + 1][0] === T_LNUMBER || $tokens[$i + 1][0] === T_DNUMBER) {
+            } elseif (in_array($tokens[$i + 1][0], [T_LNUMBER, T_DNUMBER])) {
                 $fullMessage .= $tokens[$i + 1][1];
             } else {
                 return null;
