b4281f80e2e0b9cbb6bb9ad8440e862937a983c6
DarkDef <darkdef@gmail.com>
2021-04-05 19:12:46 +0300

    Add function `getMessages` to reader interface

--- src/MessageReaderInterface.php
+++ src/MessageReaderInterface.php
@@ -21,4 +21,29 @@ interface MessageReaderInterface
      * @return string|null The translated message or null if translation wasn't found.
      */
     public function getMessage(string $id, string $category, string $locale, array $parameters = []): ?string;
+
+    /**
+     * @param string $category Category of the message to get.
+     * @param string $locale Locale of the message to get.
+     *
+     * @psalm-return array<string, array<string, string>>
+     *
+     * @return array A get of all messages from category. The format is the following:
+     *
+     * ```php
+     * [
+     *   'key1' => [
+     *     'message' => 'translation1',
+     *     // Extra metadata that writer may use:
+     *     'comment' => 'Translate carefully!',
+     *   ],
+     *   'key2' => [
+     *     'message' => 'translation2',
+     *     // Extra metadata that writer may use:
+     *     'comment' => 'Translate carefully!',
+     *   ],
+     * ]
+     * ```
+     */
+    public function getMessages(string $category, string $locale): array;
 }
