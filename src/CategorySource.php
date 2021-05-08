65a4976ef26e41bdf6b1d9864e363161a89d4f7d
Alexander Makarov <samdark@users.noreply.github.com>
2021-05-08 16:46:07 +0000

    Apply fixes from StyleCI

--- src/CategorySource.php
+++ src/CategorySource.php
@@ -47,7 +47,6 @@ final class CategorySource
      *
      * @param string $message Message to be formatted.
      * @param array $parameters Parameters to use.
-     *
      * @param string $locale Locale to use. Usually affects formatting numbers, dates etc.
      *
      * @return string Formatted message.
