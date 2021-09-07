ad7b7d51fbd6af9234f716c78ddb473454180221
Alexander Makarov <sam@rmcreative.ru>
2021-09-08 00:06:05 +0300

    Revert "Apply fixes from StyleCI"
    This reverts commit a90edcbbb4a65eb27eabb25968a1d12461cf103d.

--- tests/extractorExamples/synthetic/correctSamples/test-static.php
+++ tests/extractorExamples/synthetic/correctSamples/test-static.php
@@ -1,7 +1,6 @@
 <?php
-
-declare(strict_types=1);
-$translator::translate('messageId1', [1, 2]);
+$translator::translate('messageId1', array(1, 2));
 $translator::translate('messageId2', [1, 2]);
-$translator::translate('message' . 'Id3', [1, 2]);
+$translator::translate('message' . 'Id3', array(1, 2));
 $translator::translate('message' . 'Id4', [1, 2]);
+
