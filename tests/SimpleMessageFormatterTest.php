d48079bd3ce8cb64f0cafc03d708561ae5250edc
Alexey Rogachev <arogachev90@gmail.com>
2022-09-07 15:49:24 +0600

    Add more data sets and improve their structure / descriptions

--- tests/SimpleMessageFormatterTest.php
+++ tests/SimpleMessageFormatterTest.php
@@ -12,26 +12,50 @@ class SimpleMessageFormatterTest extends TestCase
     public function formatProvider(): array
     {
         return [
-            [
+            // simple
+            'simple, scalar (integer)' => [
                 'Test number: {number}',
                 ['number' => 5],
                 'Test number: 5',
             ],
-            [
+            'simple, scalar (string)' => [
                 'Test string: {str}',
                 ['str' => 'string data'],
                 'Test string: string data',
             ],
-            [
+            'simple, non-scalar (array)' => [
                 'Test array: {arr}',
                 ['arr' => ['string data']],
                 'Test array: {arr}',
             ],
             // plural
-            [
-                '{min} {min, number} {min, plural, one{character} other{characters}} {max}',
-                ['min' => 2, 'max' => 1],
-                '2 2 2 characters 1',
+            'plural, one' => [
+                '{min, plural, one{character} other{characters}}',
+                ['min' => 1],
+                '1 character',
+            ],
+            'plural, other' => [
+                '{min, plural, one{character} other{characters}}',
+                ['min' => 2],
+                '2 characters',
+            ],
+            'plural, reversed options' => [
+                '{min, plural, other{characters} one{character}}',
+                ['min' => 1],
+                '1 character',
+            ],
+            // not supported
+            'not supported' => [
+                '{min, notsupported}',
+                ['min' => 1],
+                '1',
+            ],
+            // complex
+            'complex' => [
+                'text1 {param1} text2 {param2, number} text3 {param3, plural, one{item} other{items}} text4 {param4} ' .
+                'text5',
+                ['param1' => 1, 'param2' => 2, 'param3' => 3, 'param4' => 4],
+                'text1 1 text2 2 text3 3 items text4 4 text5',
             ],
         ];
     }
