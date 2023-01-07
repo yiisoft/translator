30ee3c103fbe6332e978376b430c2d06968c7191
darkdef <darkdef@gmail.com>
2023-01-07 23:28:17 +0300

    Changed default value for locale to BCP-47 compatible

--- config/params.php
+++ config/params.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'yiisoft/translator' => [
-        'locale' => 'en_US',
+        'locale' => 'en-US',
         'fallbackLocale' => null,
         'defaultCategory' => 'app',
     ],
